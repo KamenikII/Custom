29c3263dcf78b7332d7416d80f252eba9604d970
Ilya Kamenik <kamenikii@yandex.ru>
2023-07-04 12:39:31 +0300

    Animation_1

--- app/src/main/java/ru/netology/customviews/ui/MainActivity.kt
+++ app/src/main/java/ru/netology/customviews/ui/MainActivity.kt
@@ -1,18 +1,94 @@
 package ru.netology.customviews.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.customviews.R
 
+@SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val view = findViewById<StatsView>(R.id.stats)
+        view.postDelayed({
+            view.data = listOf(
+                500F,
+                500F,
+                500F,
+                500F,
+            )
+        }, 50)
+//
+//        val textView = findViewById<TextView>(R.id.label)
+//
+//        val viewAnim = AnimationUtils.loadAnimation(
+//            this, R.anim.animation
+//        ).apply {
+//            setAnimationListener(object : Animation.AnimationListener {
+//                override fun onAnimationStart(animation: Animation?) {
+//                    textView.text = "started"
+//                }
+//
+//                override fun onAnimationEnd(animation: Animation?) {
+//                    textView.text = "ended"
+//                }
+//
+//                override fun onAnimationRepeat(animation: Animation?) {
+//                    textView.text = "repeat"
+//                }
+//            })
+//        }
+//
+//        view.startAnimation(viewAnim)
 
-        findViewById<StatsView>(R.id.stats).data = listOf(
-            500F,
-            500F,
-            500F,
-            500F,
-        )
+//        ObjectAnimator.ofFloat(view, "alpha", 0.25F, 1F).apply {
+//            startDelay = 500
+//            duration = 300
+//            interpolator = BounceInterpolator()
+//        }.start()
+
+//        ObjectAnimator.ofFloat(view, View.ALPHA, 0.25F, 1F).apply {
+//            startDelay = 500
+//            duration = 300
+//            interpolator = BounceInterpolator()
+//        }.start()
+
+//        val rotation = PropertyValuesHolder.ofFloat(View.ROTATION, 0F, 360F)
+//        val alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 0F, 1F)
+//        ObjectAnimator.ofPropertyValuesHolder(view, rotation, alpha)
+//            .apply {
+//                startDelay = 500
+//                duration = 500
+//                interpolator = LinearInterpolator()
+//            }.start()
+//
+//        view.animate()
+//            .rotation(360F)
+//            .scaleX(1.2F)
+//            .scaleY(1.2F)
+//            .setInterpolator(LinearInterpolator())
+//            .setStartDelay(500)
+//            .setDuration(500)
+//            .start()
+
+//        val alpha = ObjectAnimator.ofFloat(view, View.ALPHA, 0.25F, 1F).apply {
+//            duration = 300
+//            interpolator = LinearInterpolator()
+//        }
+//        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0F, 1F)
+//        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0F, 1F)
+//        val scale = ObjectAnimator.ofPropertyValuesHolder(view, scaleX, scaleY).apply {
+//            duration = 300
+//            interpolator = BounceInterpolator()
+//        }
+//        AnimatorSet().apply {
+//            startDelay = 500
+//            playSequentially(scale, alpha)
+//        }.start()
     }
 }
--- app/src/main/java/ru/netology/customviews/ui/StatsView.kt
+++ app/src/main/java/ru/netology/customviews/ui/StatsView.kt
@@ -1,5 +1,6 @@
 package ru.netology.customviews.ui
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -7,6 +8,7 @@ import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.content.withStyledAttributes
 import ru.netology.customviews.R
 import ru.netology.customviews.util.AndroidUtils
@@ -25,26 +27,29 @@ class StatsView @JvmOverloads constructor(
     defStyleAttr,
     defStyleRes
 ) {
-    private fun smartCustom(sum: Float): Float =
-        if (sum < 1) 1F else sum.pow(-1)
-    private fun generateRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
-
     //данные
     var data: List<Float> = emptyList()
         set(value) {
             field = value
-            invalidate()
+            update()
         }
 
     //рисуем кружочек
     private var radius = 0F
     private var center = PointF(0F, 0F)
     private var oval = RectF(0F, 0F, 0F, 0F)
-
     //цвета, ширина линии
     private var lineWidth = AndroidUtils.dp(context, 5F).toFloat()
     private var fontSize = AndroidUtils.dp(context, 40F).toFloat()
     private var colors = emptyList<Int>()
+
+    //от куда стартуем
+    private var startFrom = -90F
+    private var progress = 0F
+
+    //анимация
+    private var animator: ValueAnimator? = null
+    private val animatorDuration= 3000L
 
     init {
         context.withStyledAttributes(
@@ -100,37 +105,102 @@ class StatsView @JvmOverloads constructor(
             return
         }
 
-        //от куда стартуем
-        var startFrom = -90F
+//        animator = ValueAnimator.ofFloat(0F, 1F)
+//            .apply {
+//                addUpdateListener { anim ->
+//                    progress = anim.animatedValue as Float
+//                    invalidate()
+//                }
+//                duration = 3600
+//                interpolator = LinearInterpolator()
+//            }.also {
+//                it.start()
+//            }
+//
+//
+//        animator = ValueAnimator.ofFloat(90F, 90F + 360F)
+//            .apply {
+//                addUpdateListener { anim ->
+//                    startFrom = anim.animatedValue as Float
+//                    invalidate()
+//                }
+//                duration = 3600
+//                interpolator = LinearInterpolator()
+//            }.also {
+//                it.start()
+//            }
+
+        data.forEachIndexed { index, datum ->
+            val angle = datum * 360F * smartCustom(data.sum())
+            paint.color = colors.getOrElse(index) { randomColor() }
+            canvas.drawArc(oval, startFrom + progress * 360F, angle * progress, false, paint)
+            startFrom += angle
+        }.apply {
+            if (progress == 1F) {
+                paint.color = colors[0]
+                canvas.drawPoint(center.x, center.y - radius, paint)
+            }
+        }
 
         //рисуем процентики в кружочке
-        for ((index, datum) in data.withIndex()) {
-            val angle = 360F * datum * smartCustom(data.sum())
-            paint.color = colors.getOrNull(index) ?: randomColor()
-            canvas.drawArc(
-                oval,
-                startFrom,
-                angle,
-                false,
-                paint)
-            startFrom += angle
-        }
+//        for ((index, datum) in data.withIndex()) {
+//            val angle = 360F * datum
+//            paint.color = colors.getOrNull(index) ?: randomColor()
+//            canvas.drawArc(
+//                oval,
+//                startFrom + (progress * 360F * datum * smartCustom(data.sum())),
+//                angle * progress,
+//                false,
+//                paint)
+//            startFrom += angle
+//        }
 
         //делаем текст
         canvas.drawText(
-            "%.2f%%".format(data.sum() * 100 * smartCustom(data.sum())),
+            "%.2f%%".format(progress * data.sum() * 100 * smartCustom(data.sum())),
             center.x,
             center.y + textPaint.textSize / 4,
             textPaint,
         )
 
         //для красивск
-        val lastDot = 1F
-        paint.color = colors.getOrElse(0) { randomColor() }
-        canvas.drawArc(oval, startFrom, lastDot, false, paint)
+        if ((progress) == 1F) {
+            val lastDot = 1F
+            paint.color = colors.getOrElse(0) { randomColor() }
+            canvas.drawArc(oval, startFrom, lastDot, false, paint)
+        }
 
     }
 
     //рандомные цвета
     private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+
+    private fun smartCustom(sum: Float): Float =
+        if (sum < 1) 1F else sum.pow(-1)
+
+//    private fun update() {
+//        animator?.let {
+//            it.removeAllListeners()
+//            it.cancel()
+//        }
+//    }
+
+    private fun update() {
+        animator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+
+        animator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = animatorDuration
+            interpolator = LinearInterpolator()
+        }.also {
+            it.start()
+        }
+    }
 }
